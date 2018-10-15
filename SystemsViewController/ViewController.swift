c75d79034bbc56d3b4ce4d08d6fcd4e63807a3a5
Reid, Jon Carl <jreid2@hk220-mac1.local>
2018-10-15 16:24:14 -0700

    Initial Commit

---
+++ SystemsViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SystemsViewController
+//
+//  Created by Reid, Jon Carl on 10/15/18.
+//  Copyright © 2018 Reid, Jon Carl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SystemsViewControllerTests/SystemsViewControllerTests.swift
@@ -0,0 +1,34 @@
+//
+//  SystemsViewControllerTests.swift
+//  SystemsViewControllerTests
+//
+//  Created by Reid, Jon Carl on 10/15/18.
+//  Copyright © 2018 Reid, Jon Carl. All rights reserved.
+//
+
+import XCTest
+@testable import SystemsViewController
+
+class SystemsViewControllerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
